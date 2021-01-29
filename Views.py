a0cd9ccfe3715fb7c530406273eadbb1adf67c44
Often-Anonymous <78204021+Often-Anonymous@users.noreply.github.com>
2021-01-29 15:52:19 +0530

    Create Views.py

---
+++ Views.py
@@ -0,0 +1,16 @@
+import time 
+from selenium import webdriver
+
+#Time to refresh page(in seconds)
+Timer = 1
+
+#Video's link
+
+link = "Your video's link goes here"
+
+browser.get(link)
+
+number_of_views =  "number of views you need goes here"
+
+driver.refresh(number_of_views)
+
